fd5c1a43a54bbc3e60d26b5cc8be3b83a875b312
Gianfranco Elli <gianelli99@hotmail.com>
2022-11-15 12:18:51 -0300

    polish meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -11,19 +11,21 @@ import {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   viewport: "width=device-width,initial-scale=1",
-  title: "Gianfranco Elli - Software developer.",
+  title: "Gianfranco Elli - Software Developer.",
   description:
     "I love spending time looking 'underneath the hood' to understand the magic behind the scenes.",
+  keywords: "Personal website, Portfolio",
   "og:type": "website",
   "og:url": "https://gianelli.vercel.app/",
-  "og:title": "Gianfranco Elli - Software developer.",
+  "og:title": "Gianfranco Elli - Software Developer.",
+  "og:site_name": "Gianfranco Elli - Software Developer.",
   "og:description":
     "I love spending time looking 'underneath the hood' to understand the magic behind the scenes.",
   "og:image": "https://gianelli.vercel.app/images/og-image.png",
   "twitter:card": "summary_large_image",
   "twitter:creator": "@GianElli99",
-  "twitter:url": "https://gianelli.vercel.app",
-  "twitter:title": "Gianfranco Elli - Software developer.",
+  "twitter:site": "@GianElli99",
+  "twitter:title": "Gianfranco Elli - Software Developer.",
   "twitter:description":
     "I love spending time looking 'underneath the hood' to understand the magic behind the scenes.",
   "twitter:image": "https://gianelli.vercel.app/images/og-image.png",
