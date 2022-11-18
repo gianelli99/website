af9539248c05ba189b3bf97bd3e2702ed061bc6a
Gianfranco Elli <gianelli99@hotmail.com>
2022-11-18 18:48:24 -0300

    dumb navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+  return (
+    <nav className="h-24">
+      <a href="/">Home</a>
+      <a href="/projects">Projects</a>
+      <a href="/blogs">Blogs</a>
+      <a href="/summary">Summary</a>
+    </nav>
+  );
+};
