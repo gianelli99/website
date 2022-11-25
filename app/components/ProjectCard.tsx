c64b0af9efbfb78a71827f06ddd85a805814398f
Gianfranco Elli <gianelli99@hotmail.com>
2022-11-25 14:06:40 -0300

    Add project card

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,3 +1,25 @@
-export const ProjectCard = () => {
-  return <h2>Projectt</h2>;
+import type { projects } from "~/data/projects";
+
+interface ProjectCardProps {
+  project: Omit<typeof projects[number], "id">;
+}
+
+export const ProjectCard = ({ project }: ProjectCardProps) => {
+  return (
+    <div className="w-72 h-44 rounded-2xl bg-maroon-50/20 border-maroon/10 border-[3px]">
+      <span>{project.category}</span>
+      <h3>{project.title}</h3>
+      {project.technologies.map((tech) => (
+        <TechDescription key={tech.id} tech={tech} />
+      ))}
+    </div>
+  );
+};
+
+interface TechDescriptionProps {
+  tech: Omit<typeof projects[number]["technologies"][number], "id">;
+}
+
+const TechDescription = ({ tech }: TechDescriptionProps) => {
+  return <div>{tech.name}</div>;
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,37 @@
 import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import {
   PersonalInfoCard,
   links as personalInfoCardLinks,
 } from "~/components/PersonalInfo";
+import { ProjectCard } from "~/components/ProjectCard";
+import { projects } from "~/data/projects";
 
 export const links: LinksFunction = () => {
   return [...personalInfoCardLinks()];
 };
 
+export const loader = () => {
+  const featuredProjects = projects.filter((p) => p.featured);
+  return json({
+    projects: featuredProjects,
+  });
+};
+
 export default function Index() {
+  const { projects } = useLoaderData<typeof loader>();
   return (
-    <div className="max-w-2xl mx-auto">
-      <main>
-        <PersonalInfoCard />
-        <div>Favorite Projects</div>
-      </main>
-    </div>
+    <>
+      <PersonalInfoCard />
+      <section className="pt-12">
+        <h2 className="text-2xl mb-3">Favorite Projects</h2>
+        <div className="flex gap-3 flex-wrap">
+          {projects.map((p) => (
+            <ProjectCard key={p.id} project={p} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
