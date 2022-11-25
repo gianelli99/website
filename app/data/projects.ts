aaf6c6c094f896048e1c3ac65b5ce865174f887c
Gianfranco Elli <gianelli99@hotmail.com>
2022-11-25 14:06:13 -0300

    add some projects

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,1 +1,76 @@
-export const projects = [{}];
+export const projects = [
+  {
+    featured: true,
+    id: "binance-trading-bot",
+    title: "Binance tradign BOT",
+    category: "Web",
+    technologies: [
+      {
+        id: "nodejs",
+        icon: "/iconicon",
+        name: "Node.js",
+      },
+      {
+        id: "telegram",
+        icon: "/iconicon",
+        name: "Telegram API",
+      },
+    ],
+  },
+
+  {
+    featured: true,
+    id: "recursion-joke",
+    title: "This website",
+    category: "Recursion 😄",
+    technologies: [
+      {
+        id: "nodejs",
+        icon: "/iconicon",
+        name: "Node.js",
+      },
+      {
+        id: "telegram",
+        icon: "/iconicon",
+        name: "Telegram API",
+      },
+    ],
+  },
+  {
+    featured: true,
+    id: "binance-trading-bot",
+    title: "Binance tradign BOT",
+    category: "Web",
+    technologies: [
+      {
+        id: "nodejs",
+        icon: "/iconicon",
+        name: "Node.js",
+      },
+      {
+        id: "telegram",
+        icon: "/iconicon",
+        name: "Telegram API",
+      },
+    ],
+  },
+
+  {
+    featured: true,
+    id: "recursion-joke",
+    title: "This website",
+    category: "Recursion 😄",
+    technologies: [
+      {
+        id: "nodejs",
+        icon: "/iconicon",
+        name: "Node.js",
+      },
+      {
+        id: "telegram",
+        icon: "/iconicon",
+        name: "Telegram API",
+      },
+    ],
+  },
+];
