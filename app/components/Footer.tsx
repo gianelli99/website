83371e484e190473172559bf8bf91603384f2012
Gianfranco Elli <gianelli99@hotmail.com>
2022-12-23 17:04:21 -0300

    footer work

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,28 @@
 export const Footer = () => {
   return (
-    <footer className="max-w-2xl mx-auto">
+    <footer className="max-w-2xl mx-auto mt-10">
       <hr className="border-1 border-maroon/10" />
-      Footer
+      <p className="my-12 text-center">
+        Made with love ❤. I took inspiration from{" "}
+        <a
+          href="https://leerob.io/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-800 hover:underline"
+        >
+          Lee Robinson
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.lydiahallie.io/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-800 hover:underline"
+        >
+          Lydia Hallie
+        </a>
+        's personal sites.
+      </p>
     </footer>
   );
 };
