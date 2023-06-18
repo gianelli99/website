d6a1bf75c556ccb2729e0ed77c2df056d8d2e01b
Gianfranco Elli <gianelli99@hotmail.com>
2023-06-18 10:44:25 -0300

    Start using RadixUI and add sitemap

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,8 @@
+import { CaretDownIcon } from "@radix-ui/react-icons";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { NavLink } from "@remix-run/react";
-import { useEffect, useRef, useState } from "react";
 
 export const NavBar = () => {
-  const [showResumeOptions, setShowResumeOptions] = useState(false);
-  const button = useRef(null);
   const links = [
     {
       to: "/",
@@ -19,88 +18,50 @@ export const NavBar = () => {
     },
   ];
 
-  useEffect(() => {
-    const closeDropdown = (e: MouseEvent) => {
-      if (e.composedPath()[0] === button.current) {
-        return;
-      }
-      setShowResumeOptions(false);
-    };
-
-    document.addEventListener("click", closeDropdown);
-
-    return () => {
-      document.removeEventListener("click", closeDropdown);
-    };
-  }, []);
-
   return (
-    <nav className="py-6 max-w-2xl mx-auto flex items-center">
-      <ul className="flex flex-wrap">
-        {links.map((link) => (
-          <li key={link.to}>
-            <NavLink
-              to={link.to}
-              prefetch="intent"
-              className={({ isActive }) =>
-                `${
-                  isActive ? "font-semibold" : "font-normal"
-                } px-3 py-1 pb-[6px] text-gray-800 hover:bg-maroon-50/20 rounded block`
-              }
-            >
-              {link.content}
-            </NavLink>
-          </li>
+    <NavigationMenu.Root className="py-6 px-2 max-w-2xl mx-auto items-center">
+      <NavigationMenu.List className="flex flex-wrap flex-col sm:flex-row">
+        {links.map(({ content, to }) => (
+          <NavigationMenu.Item key={to}>
+            <NavigationMenu.Link asChild>
+              <NavLink
+                to={to}
+                className={`aria-[current]:underline px-3 py-1 pb-[6px] text-gray-800 hover:bg-maroon-50/20 rounded block`}
+              >
+                {content}
+              </NavLink>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
         ))}
-        <li className="relative">
-          <button
-            ref={button}
-            className="inline-flex w-full justify-center items-center rounded-md border-maroon/10 border-2 px-2 pb-1 pt-[2px] hover:bg-maroon-50/20"
-            id="menu-button"
-            aria-expanded="true"
-            aria-haspopup="true"
-            onClick={() => setShowResumeOptions(!showResumeOptions)}
-          >
-            Resume
-            <svg
-              className="-mr-1 ml-2 mt-1 h-5 w-5 pointer-events-none"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
-          {showResumeOptions && (
-            <div
-              className="absolute right-0 z-10 mt-2 w-24 origin-top-right rounded-md bg-white shadow-lg"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="menu-button"
-            >
-              <a
-                href="images/cv/Gianfranco Elli - Resume.pdf"
-                download
-                className="block p-2 hover:bg-maroon-50/20"
-              >
-                English
-              </a>
-              <a
-                href="images/cv/Gianfranco Elli - Curriculum Vitae.pdf"
-                download
-                className="block p-2 hover:bg-maroon-50/20"
-              >
-                Spanish
-              </a>
-            </div>
-          )}
-        </li>
-      </ul>
-    </nav>
+
+        <NavigationMenu.Item className="relative">
+          <NavigationMenu.Trigger className="inline-flex w-full justify-center items-center rounded-md border-maroon/10 border-2 px-2 pb-1 pt-[2px] hover:bg-maroon-50/20">
+            Resume <CaretDownIcon aria-hidden />
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <ul className="absolute right-0 left-0 rounded p-2 mt-1 bg-white">
+              <li>
+                <NavigationMenu.Link
+                  href="images/cv/Gianfranco Elli - Resume.pdf"
+                  className="block p-2 hover:bg-maroon-50 rounded"
+                  download
+                >
+                  English
+                </NavigationMenu.Link>
+              </li>
+              <li>
+                <NavigationMenu.Link
+                  href="images/cv/Gianfranco Elli - Curriculum Vitae.pdf"
+                  className="block p-2 hover:bg-maroon-50 rounded"
+                  download
+                >
+                  Spanish
+                </NavigationMenu.Link>
+              </li>
+            </ul>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
   );
 };
