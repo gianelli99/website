b1f4a9a987b435de1517ce7714979c2d56afd1c9
Gianfranco Elli <gianelli99@hotmail.com>
2024-07-14 15:03:51 -0300

    Update work experience

--- app/components/WorkCard.tsx
+++ app/components/WorkCard.tsx
@@ -1,12 +1,12 @@
 import type { work } from "~/data/work";
 
 interface WorkCardProps {
-  work: Omit<typeof work[number], "id">;
+  work: Omit<(typeof work)[number], "id">;
 }
 
 export const WorkCard = ({ work }: WorkCardProps) => {
   return (
-    <div className="mb-6 p-6 rounded-md bg-maroon-50/20 border border-maroon/10">
+    <div className="mb-6 p-5 rounded-md bg-maroon-50/20 border border-maroon/10">
       <a
         href={work.link}
         target="_blank"
@@ -22,10 +22,10 @@ export const WorkCard = ({ work }: WorkCardProps) => {
         />
       </a>
       <p className="text-gray-600 text-xs mt-4">
-        FROM {work.date.from.toUpperCase()} TO {work.date.to.toUpperCase()}
+        {work.date.from.toUpperCase()} - {work.date.to.toUpperCase()}
       </p>
       <p className="font-bold text-lg">{work.role}</p>
-      <p className="mt-4">{work.description}</p>
+      <p className="mt-2">{work.description}</p>
       <ul className="list-disc list-inside mt-2">
         {work.keyAchivements.map((achivement, idx) => (
           <li key={idx}>{achivement}</li>
